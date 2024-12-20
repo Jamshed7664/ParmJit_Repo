3d22431d738bc1ade1fb5eadb621fe9f79067b80
Jamshed Ahmad <jamshed7664@gmail.com>
2024-12-21 00:14:48 +0530

    add update form and validation in create form

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 
-document.getElementById('detail_card').style.display = 'none'
-document.getElementById('form_detail').style.display = 'block'
+var errorMessage = '';
 
+function onOnLoadCOntent() {
+    document.getElementById('detail_card').style.display = 'none';
+    document.getElementById('form_detail').style.display = 'block';
+    document.getElementById('update_form_detail').style.display = 'none';
+
+    document.getElementById('fname').focus();
+    document.getElementById('alert').style.display = 'none';
+    document.getElementById('alert_Error').style.display = 'none';
+
+}
 
 function submitForm() {
     let firstName = document.getElementById('fname').value;
@@ -12,29 +21,50 @@ function submitForm() {
     let address = document.getElementById('address').value;
 
     if (!firstName) {
-        alert("First name required")
+        errorMessage = "First name required";
+        document.getElementById('error_message').innerHTML = errorMessage;
+        document.getElementById('alert_Error').style.display = 'block';
     } else if (!lastName) {
-        alert("Last name required")
+        errorMessage = "Last name required";
+        document.getElementById('error_message').innerHTML = errorMessage;
+        document.getElementById('alert_Error').style.display = 'block';
     } else if (!email) {
-        alert("email required")
+        errorMessage = "email required";
+        document.getElementById('error_message').innerHTML = errorMessage;
+        document.getElementById('alert_Error').style.display = 'block';
     } else if (!password) {
-        alert("Password required")
+        errorMessage = "Password required";
+        document.getElementById('error_message').innerHTML = errorMessage;
+        document.getElementById('alert_Error').style.display = 'block';
     } else if (!confirmPassword) {
-        alert("Confirm Password required")
-    } else if (!address) {
-        alert("Address required")
-    } else if (password !== confirmPassword) {
-        alert("Password does not match")
+        errorMessage = "Confirm Password required";
+        document.getElementById('alert_Error').style.display = 'block';
+        document.getElementById('error_message').innerHTML = errorMessage
     }
+        else if (password !== confirmPassword) {
+        errorMessage = "Password does not match";
+        document.getElementById('error_message').innerHTML = errorMessage;
+        document.getElementById('alert_Error').style.display = 'block';
+
+    }
+    else if (!address) {
+        errorMessage = "Address required";
+        document.getElementById('error_message').innerHTML = errorMessage;
+        document.getElementById('alert_Error').style.display = 'block';
+    } 
     else {
-        alert("Form submitted successfully.");
-        document.getElementById('form_detail').style.display = 'none';
-        document.getElementById('detail_card').style.display = 'block';
-        
-        document.getElementById('pFname').innerHTML ="First name : "+ firstName;
-        document.getElementById('plname').innerHTML ="Last name : "+ lastName;
-        document.getElementById('pEmail').innerHTML ="Email : "+ email;
-        document.getElementById('pAddress').innerHTML ="Address : "+ address;
+        setTimeout(() => {
+            document.getElementById('form_detail').style.display = 'none';
+            document.getElementById('detail_card').style.display = 'block';
+        }, 2000);
+        document.getElementById('alert_Error').style.display = 'none'; 
+
+        document.getElementById('alert').style.display = 'block';
+
+        document.getElementById('pFname').innerHTML = "First name : " + firstName;
+        document.getElementById('plname').innerHTML = "Last name : " + lastName;
+        document.getElementById('pEmail').innerHTML = "Email : " + email;
+        document.getElementById('pAddress').innerHTML = "Address : " + address;
     }
 
 }
@@ -42,11 +72,32 @@ function submitForm() {
 function onCancel() {
     document.getElementById('detail_card').style.display = 'none';
     document.getElementById('form_detail').style.display = 'block';
-
+    document.getElementById('alert').style.display = 'none';
     document.getElementById('fname').value = '';
     document.getElementById('lname').value = '';
     document.getElementById('email').value = '';
     document.getElementById('password').value = '';
     document.getElementById('confirmPassword').value = '';
     document.getElementById('address').value = '';
+}
+
+
+// Code for update form
+
+// function to view update form
+
+
+function viewUpdateForm() {
+    document.getElementById('update_form_detail').style.display = 'block';
+     document.getElementById('detail_card').style.display = 'none';
+    document.getElementById('form_detail').style.display = 'none';
+    
+}
+
+function updateForm() {
+    
+}
+
+function onCancelUpdateForm() {
+    
 }

439b7f334d8a7d3ac3479892bd42f0d3072a7cd7
Jamshed Ahmad <jamshed7664@gmail.com>
2024-12-22 00:08:52 +0530

    added new callback file

---
+++ Callback Function/callback.js
@@ -0,0 +1,86 @@
+
+// function multiply(result) {
+//     console.log("Result : ", result);
+//     for (let i = 1; i <= 10; i++){
+//         console.log(result +'*'+ i+' = '+result*i )
+//     }
+// }
+
+
+// function add(a, b,myFn) {
+//     let sum = a + b;
+//     // myFn = fn2 ;
+//     myFn(sum);
+
+// }
+// add(2, 3, multiply);
+
+
+
+// async function getPostData() {
+//     let data = await fetch('https://jsonplaceholder.typicode.com/posts', {
+//         method: 'GET'
+//     });
+//     let result = await data.json();
+//     console.log(result);
+//     myData(result)
+
+// }
+// getPostData();
+
+// function myData(data) {
+//     for (let i = 0; i < data.length; i++){
+//         console.log(data[i].id);
+//     }
+// }
+
+// Arrow function
+
+// function result() {
+//     console.log('My result')
+// }
+// result();
+
+// by using arrow function
+
+// let myResult = ()=> console.log('My result in arrow function');
+// myResult()
+
+
+// let add = (a, b) => a + b;
+
+// console.log(add(2, 3))
+
+
+// let add = (a, b) => {
+//     let sum = a + b;
+//     // return sum;
+//     console.log('Result of given number is',sum)
+
+// }
+// console.log('Result of given number is',add(2, 3))
+
+
+let arr = [1,2, 3, 4, 5,];
+
+// let result = arr.map(e => e * 2);
+// console.log('Result',result);
+// console.log('Actual array', arr);
+
+
+// let deletElement = arr.splice(2, 1);
+
+// console.log('DeletElement', deletElement);
+// console.log('Actual array', arr);
+
+
+// let findElement = arr.find(e => e > 5)
+// console.log(findElement)
+
+// let findElement = arr.some(e => e > 5)
+// console.log(findElement)
+
+// arr.shift()
+// console.log(arr)
+
+
